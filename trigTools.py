4d7179aa7d5e61458c6b940f931d06640ba41f57
Kameron (Eliza) <kpcpalmer@gmail.com>
2019-06-04 21:00:04 -0400

    Fixed mistake in converting units to radian.

--- trigTools.py
+++ trigTools.py
@@ -1,8 +1,8 @@
 import numpy
 
 def sind( degAngle ):
-	return numpy.sin( 2.*numpy.pi / 180. * degAngle )
+	return numpy.sin( numpy.pi / 180. * degAngle )
 #
 def cosd( degAngle ):
-	return numpy.cos( 2.*numpy.pi / 180. * degAngle )
+	return numpy.cos( numpy.pi / 180. * degAngle )
 #
