a1041809b06c99b2e71c94531cc3e2cc13d8b545
Kameron (Catan) <kpcpalmer@gmail.com>
2019-07-10 19:10:28 -0400

    Added rotation matricies

--- algebraTools.py
+++ algebraTools.py
@@ -3,3 +3,18 @@ import numpy
 def skewSymmetricMatrix(vector):
 	return numpy.array( [[0, -vector[2], vector[1]], [vector[2],0,-vector[0]], [-vector[1],vector[0],0] ] )
 #
+def rotationX( t ):
+	c = numpy.cos(t)
+	s = numpy.sin(t)
+	return numpy.asarray( [[1,0,0], [0,c,-s], [0,s,c]] )
+#
+def rotationY( t ):
+	c = numpy.cos(t)
+	s = numpy.sin(t)
+	return numpy.asarray( [[c,0,s],[0,1,0],[-s,0,c]] )
+#
+def rotationZ( t ):
+	c = numpy.cos(t)
+	s = numpy.sin(t)
+	return numpy.asarray( [[c,-s,0],[s,c,0],[0,0,1]] )
+#
