df909a6f11880009f82567777769349a46697230
Kameron (Catan) <kpcpalmer@gmail.com>
2019-08-04 15:26:53 -0400

    updated create poly coeffs functions.

--- polynomialTools.py
+++ polynomialTools.py
@@ -1,16 +1,38 @@
 import numpy
 
-def makePoly_Q0V0A0QTVT( q0, v0, a0, qt, vt):
-	A = numpy.array([	[1,0,0,0,0],
-						[0,1,0,0,0],
-						[0,0,2,0,0],
-						[1,1,1,1,1],
-						[0,1,2,3,4]
+def polyCoeffs_q0v0a0( T, q0, v0, a0 ):
+	A = numpy.array([	[1,		0,		0 ],
+						[0,		1.,		0 ],
+						[0,		0,		2 ]
 					])
-	
-	Q = numpy.array([q0, v0   , a0      , qt, vt   ])
-	return numpy.matmul( numpy.linalg.inv( A ), Q )
+	Q = numpy.array([q0, v0, a0])
+	return numpy.flip( numpy.matmul( numpy.linalg.inv(A), Q ) )
 #
-def makePoly_Q0V0A0( q0, v0, a0 ):
-	return numpy.array([q0, v0, 0.5*a0])
+def polyCoeffs_q0v0a0qT( T, q0, v0, a0, qT ):
+	A = numpy.array([	[1,	0,	0,		0],
+						[0,	1,	0,		0],
+						[0,	0,	2,		0],
+						[1,	T,	T**2,	T**3]
+					])
+	Q = numpy.array([q0, v0, a0, qT])
+	return numpy.flip( numpy.matmul( numpy.linalg.inv(A), Q ) )
+#
+def polyCoeffs_q0v0a0qTvT( T, q0, v0, a0, qT, vT ):
+	A = numpy.array([	[1,	0,	0,		0,		0],
+						[0, 1,	0,		0,		0],
+						[0,	0,	2,		0,		0],
+						[1,	T,	T**2,	T**3,	T**4],
+						[0,	1,	2*T,	3*T**2,	4*T**3]
+					])
+	Q = numpy.array([q0, v0, a0, qT, vT])
+	return numpy.flip( numpy.matmul( numpy.linalg.inv(A), Q ) )
+#
+def polyCoeffs_q0v0a0vT( T, q0, v0, a0, vT ):
+	A = numpy.array([	[1,	0,	0,	0],
+						[0,	1,	0,	0],
+						[0,	0,	2,	0],
+						[0,	1,	2*T,3*T**3]
+				])
+	Q = numpy.array([q0, v0, a0, vT])
+	return numpy.flip( numpy.matmul( numpy.linalg.inv(A), Q ) )
 #
--- unittests.py
+++ unittests.py
@@ -2,19 +2,19 @@ import numpy
 import numpy.random
 import unittest
 
-from polynomialTools import makePoly_Q0V0A0QTVT
-from polynomialTools import makePoly_Q0V0A0
+from polynomialTools import polyCoeffs_q0v0a0qTvT
+from polynomialTools import polyCoeffs_q0v0a0
 
 class Test_A_polynomialTools( unittest.TestCase ):
-	def test_A000_makePoly_Q0V0A0QTVT(self):
+	def test_A000_polyCoeffs_q0v0a0qT(self):
 		# no motion
 		r0 = numpy.random.random()*10 - 5
 		v0 = 0
 		a0 = 0
 		rT = r0
 		vT = 0
-		target = [r0, 0, 0, 0, 0]
-		result = makePoly_Q0V0A0QTVT(r0, v0, a0, rT, vT)
+		target = numpy.flip([r0, 0, 0, 0, 0])
+		result = polyCoeffs_q0v0a0qTvT(1, r0, v0, a0, rT, vT)
 		self.assertTrue( numpy.allclose( target, result ) )
 		
 		# no initial acceleration
@@ -23,18 +23,18 @@ class Test_A_polynomialTools( unittest.TestCase ):
 		a0 = 0
 		rT = numpy.random.random()*10 - 5
 		vT = v0
-		target = [r0, v0, 0, 4*(rT - r0 - v0), -3*(rT - r0 - v0)]
-		result = makePoly_Q0V0A0QTVT(r0, v0, a0, rT, vT)
+		target = numpy.flip([r0, v0, 0, 4*(rT - r0 - v0), -3*(rT - r0 - v0)])
+		result = polyCoeffs_q0v0a0qTvT(1, r0, v0, a0, rT, vT)
 		self.assertTrue( numpy.allclose( target, result ) )
 		
 		return
 	#
-	def test_A001_makePoly_Q0V0A0(self):
+	def test_A001_polyCoeffs_q0v0a0(self):
 		r0 = numpy.random.random()*10 - 5
 		v0 = numpy.random.random()*10 - 5
 		a0 = numpy.random.random()*10 - 5
-		target = [r0, v0, 0.5*a0]
-		result = makePoly_Q0V0A0(r0, v0, a0)
+		target = numpy.flip([r0, v0, 0.5*a0])
+		result = polyCoeffs_q0v0a0(1, r0, v0, a0)
 		self.assertTrue( numpy.allclose( target, result ) )
 		return
 	#
