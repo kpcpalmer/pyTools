d21e1dfd3d82757362158b82cb427a6c71979c2f
Kameron (Eliza) <kpcpalmer@gmail.com>
2019-09-02 10:50:09 -0400

    Added a 6 BC poly function

--- polynomialTools.py
+++ polynomialTools.py
@@ -36,3 +36,14 @@ def polyCoeffs_q0v0a0vT( T, q0, v0, a0, vT ):
 	Q = numpy.array([q0, v0, a0, vT])
 	return numpy.flip( numpy.matmul( numpy.linalg.inv(A), Q ) )
 #
+def polyCoeffs_q0v0a0qTvTaT( T, q0, v0, a0, qT, vT, aT ):
+	A = numpy.array([	[1,	0,	0,		0,		0,		0],
+						[0,	1,	0,		0,		0,		0],
+						[0,	0,	2,		0,		0,		0],
+						[1,	T,	T**2, 	T**3,	T**4,	T**5],
+						[0, 1,	2*T,	3*T**2,	4*T**3,	5*T**4],
+						[0, 0,	2,		6*T,	12*T**2,20*T**3]
+					])
+	Q = numpy.array([q0,v0,a0,qT,vT,aT])
+	return numpy.flip( numpy.matmul( numpy.linalg.inv(A), Q ) )
+#
